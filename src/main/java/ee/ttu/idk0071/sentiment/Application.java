e3482ed1f9183bd39060b969e0b1e07dc3c5cf5f
Tanel Prikk <tanel.prikk@outlook.com>
2017-02-20 22:52:24 +0200

    Renamed again

--- src/main/java/ee/ttu/idk0071/sentiment/Application.java
+++ src/main/java/ee/ttu/idk0071/sentiment/Application.java
@@ -1,4 +1,4 @@
-package ee.ttu.idk0071.reputability;
+package ee.ttu.idk0071.sentiment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/ee/ttu/idk0071/sentiment/ApplicationTests.java
+++ src/test/java/ee/ttu/idk0071/sentiment/ApplicationTests.java
@@ -1,4 +1,4 @@
-package ee.ttu.idk0071.reputability;
+package ee.ttu.idk0071.sentiment;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
