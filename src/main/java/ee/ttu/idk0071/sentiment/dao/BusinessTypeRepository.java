89ede5f19c8f789d804222fdec155b4cbaf1e380
Tanel Prikk <tanel.prikk@outlook.com>
2017-02-23 19:58:27 +0200

    Classifier repositories added

---
+++ src/main/java/ee/ttu/idk0071/sentiment/dao/BusinessTypeRepository.java
@@ -0,0 +1,7 @@
+package ee.ttu.idk0071.sentiment.dao;
+
+import ee.ttu.idk0071.sentiment.model.BusinessType;
+
+public interface BusinessTypeRepository extends ReadOnlyRepository<BusinessType, Integer>{
+
+}
---
+++ src/main/java/ee/ttu/idk0071/sentiment/dao/SentimentTypeRepository.java
@@ -0,0 +1,7 @@
+package ee.ttu.idk0071.sentiment.dao;
+
+import ee.ttu.idk0071.sentiment.model.SentimentType;
+
+public interface SentimentTypeRepository extends ReadOnlyRepository<SentimentType, String>{
+
+}
