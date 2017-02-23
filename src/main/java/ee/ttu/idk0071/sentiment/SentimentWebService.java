55063eac6fe1d4ebaeecb00ad72650b25363fd01
Tanel Prikk <tanel.prikk@outlook.com>
2017-02-23 20:05:58 +0200

    Rename: Application -> SentimentWebService

--- src/main/java/ee/ttu/idk0071/sentiment/SentimentWebService.java
+++ src/main/java/ee/ttu/idk0071/sentiment/SentimentWebService.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class SentimentWebService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(SentimentWebService.class, args);
 	}
 }
