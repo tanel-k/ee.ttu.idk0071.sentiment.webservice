0b7676ab829553877836ad5c72440b3c0c283c7d
Tanel Prikk <tanel.prikk@outlook.com>
2017-02-23 20:11:46 +0200

    Missing @Autowired

--- src/main/java/ee/ttu/idk0071/sentiment/controller/BusinessTypeController.java
+++ src/main/java/ee/ttu/idk0071/sentiment/controller/BusinessTypeController.java
@@ -2,6 +2,7 @@ package ee.ttu.idk0071.sentiment.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import ee.ttu.idk0071.sentiment.service.BusinessTypeService;
 
 @RestController
 public class BusinessTypeController {
+	@Autowired
 	private BusinessTypeService businessTypeService;
 	
 	@RequestMapping("/classifiers/business-types")
--- src/main/java/ee/ttu/idk0071/sentiment/controller/CountryController.java
+++ src/main/java/ee/ttu/idk0071/sentiment/controller/CountryController.java
@@ -2,6 +2,7 @@ package ee.ttu.idk0071.sentiment.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import ee.ttu.idk0071.sentiment.service.CountryService;
 
 @RestController
 public class CountryController {
+	@Autowired
 	private CountryService countryService;
 	
 	@RequestMapping("/classifiers/countries")
