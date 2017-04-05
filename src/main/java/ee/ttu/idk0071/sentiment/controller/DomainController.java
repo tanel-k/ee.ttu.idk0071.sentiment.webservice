9b382b7395eeec99ac0f74cfab2f2d7dafa4c476
Margus Tanneberg <m.tanneberg@gmail.com>
2017-04-05 18:52:02 +0300

    Domain Controller done

---
+++ src/main/java/ee/ttu/idk0071/sentiment/controller/DomainController.java
@@ -0,0 +1,22 @@
+package ee.ttu.idk0071.sentiment.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ee.ttu.idk0071.sentiment.model.Domain;
+import ee.ttu.idk0071.sentiment.repository.DomainRepository;
+
+@RestController
+public class DomainController {
+
+	@Autowired
+	private DomainRepository domainRepository;
+
+	@RequestMapping("/domains")
+	public List<Domain> getDomains() {
+		return domainRepository.findByActiveTrue();
+	}
+}
